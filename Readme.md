94a2360df4c0ac00c4b3f2280f1d99790470d884
jnkbldgn <buldygin.em@gmail.com>
2020-11-12 14:40:54 +0300

    added cli

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## ./cli.js
+Create component. Default create functional component withot style file.
+```
+./cli.js [path] <options>
+```
+
+|option | value                       |
+|-------|---------------------------- |
+| -c    | create class component      |
+| -s    | create component with style |
--- cli.js
+++ cli.js
@@ -1,68 +1,128 @@
 #!/usr/bin/env node
+/* eslint-disable import/no-extraneous-dependencies */
 const yargs = require('yargs/yargs');
 const fs = require('fs');
+const path = require('path');
 
 const { hideBin } = yargs;
 
-const classTemplate = `
-import React form 'react';
+const resolve = (filename) => path.resolve(__dirname, filename);
+const createdCallback = (error, filePath) => {
+  if (error) {
+    console.error(error);
+  }
+  console.info(`Created ${filePath}`);
+};
+
+const INDEX_FILE_NAME = 'index.js';
+const STYLES_FILE_NAME = 'styles.module.scss';
+const COMPONENT_EXT = 'jsx';
+
+const classTemplate = `import React from 'react';
+{styles}
 
 export default class {name} extends React.Component {
-    constructor() {
+  constructor() {
 
-    }
+  }
 
-    render() {
-        return (
-            <>
-            </>
-        );
-      }
-}
-`;
-
-const funcTemplate = `
-export default function {name}(props) {
-
-    return(
-        <>
-        </>
+  render() {
+    return (
+      <>
+      </>
     );
+  }
 }
 `;
 
-const { argv } = yargs(hideBin(process.argv))
+const stylesTemplate = `import styles from './${STYLES_FILE_NAME}';`;
+
+const funcTemplate = `{styles}
+
+export default function {name}(props) {
+  return (
+    <>
+    </>
+  );
+}
+`;
+
+const indexTemplate = `import {name} from './{name}';
+
+export default {name};
+`;
+
+// eslint-disable-next-line no-unused-vars
+const { argv: ARGV } = yargs(hideBin(process.argv))
   .command(
     'create [path]',
     'creating component',
     (yArgs) => {
-      yArgs.positional('path');
+      yArgs
+        .positional('path', {
+          alias: 'p',
+          type: 'string',
+          description: 'path to component',
+        })
+        .option(
+          'class', {
+            alias: 'c',
+            type: 'boolean',
+            default: false,
+            description: 'create class component',
+          },
+        )
+        .option(
+          'style', {
+            alias: 's',
+            type: 'boolean',
+            default: false,
+            description: 'create component with style',
+          },
+        );
     },
     (argv) => {
       const template = argv.class ? classTemplate : funcTemplate;
+      const styleReg = argv.style ? new RegExp('{styles}') : new RegExp('{styles}\\n');
+      const styleValue = argv.style ? stylesTemplate : '';
 
       if (!argv.path) {
-        process.stdout('Error. Path is empty.');
+        console.error('Error. Path is empty.');
       }
-      const name = argv.path.match(/[^/]+$/);
-      console.info(name);
-      template.replace('{name}', name);
-      fs.writeFileSync(argv.path, template.replace('{name}', name), (err) => process.stdout(err));
-    },
-  )
-  .option(
-    'class', {
-      alias: 'c',
-      type: 'boolean',
-      default: false,
-      description: 'create class component',
-    },
-  )
-  .option(
-    'style', {
-      alias: 's',
-      type: 'boolean',
-      default: false,
-      description: 'create component with style',
+      const [full, folder, file] = argv.path.match(/(^.+\/)([^/]+$)/);
+
+      if (!full || !folder || !file) {
+        console.error('Invalid path');
+      }
+      const [name, ext = COMPONENT_EXT] = file.split('.');
+
+      if (!name || !ext) {
+        console.error('Invalid filename');
+      }
+      const componentData = template.replace(/{name}/g, name).replace(styleReg, styleValue);
+      const indexData = indexTemplate.replace(/{name}/g, name);
+      fs.access(resolve(folder), (err) => {
+        if (err && err.code === 'ENOENT') {
+          switch (err.code) {
+            case 'ENOENT': {
+              fs.mkdirSync(resolve(err.path, createdCallback));
+              break;
+            }
+            default: {
+              console.error(err);
+              return;
+            }
+          }
+        }
+        const componentPath = resolve(`${folder}/${name}.${ext}`);
+        const indexPath = resolve(`${folder}${INDEX_FILE_NAME}`);
+        const stylesPath = resolve(`${folder}${STYLES_FILE_NAME}`);
+
+        fs.writeFile(componentPath, componentData, (e) => createdCallback(e, componentPath));
+        fs.writeFile(indexPath, indexData, (e) => createdCallback(e, indexPath));
+        if (argv.style) {
+          fs.writeFile(stylesPath, '', (e) => createdCallback(e, stylesPath));
+        }
+      });
     },
   );
